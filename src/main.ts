4cedcf1482bd3099dd91adb4e7733833919d989a
ThinLiquid <thnlqd@gmail.com>
2024-03-03 17:06:51 +0000

    debug (1)

--- src/main.ts
+++ src/main.ts
@@ -126,8 +126,9 @@ const load = async (songId: string): Promise<void> => {
   })
 
   for (let v = 0; 'videoId' in searchData[v]; v++) {
+    if (searchData[v].genre !== "Music") continue
+    console.log(searchData[v])
     videoData = await getYouTubeVideo((searchData[v] as SearchVideo).videoId)
-    if (videoData.genre !== "Music") continue
     if (videoData !== null) break
   }
 
